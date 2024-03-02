ec59cb11427650b6f35f9797f950865fc91992a6
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-02 21:01:06 +0300

    add builtins library

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use std::fs::File;
+use std::{fs::File, path::Path, collections::HashMap, io};
 
 use tracing::{info, debug, Level, error, trace, warn};
-use crossbeam_channel::SendError;
 use thiserror::Error;
 
-use lsp_types::{OneOf, ServerCapabilities, request::{GotoDefinition, GotoDeclarationParams, GotoDeclaration, Request}, GotoDefinitionResponse, InitializeParams, GotoDefinitionParams, ClientCapabilities, DeclarationCapability};
+use lsp_types::{OneOf, ServerCapabilities, request::{GotoDefinition, GotoDeclarationParams, GotoDeclaration, Request, HoverRequest}, GotoDefinitionResponse, InitializeParams, GotoDefinitionParams, ClientCapabilities, DeclarationCapability, Location, Url, Range, Position, HoverParams, HoverContents, MarkupContent, MarkupKind, HoverProviderCapability, MarkedString, Hover};
 use lsp_server::{Connection, ExtractError, Message, RequestId, Response, ProtocolError, ResponseError};
 
 #[derive(Error, Debug)]
@@ -21,12 +20,44 @@ enum Error {
     Send(#[from] crossbeam_channel::SendError<Message>),
     #[error("Unimplemented")]
     Unimplemented { method: String },
+    #[error("Uri parse")]
+    Uri { uri: String },
 }
+
+#[derive(Debug, Clone)]
+struct Declaration {
+    pub location: Location,
+    pub documentation: MarkupContent,
+}
+type Builtins = HashMap::<& 'static str, Declaration>;
 
 struct Server {
     connection: Connection,
     server_capabilities: ServerCapabilities,
     initialize_parameters: InitializeParams,
+    builtins: Builtins,
+}
+
+fn load_builtins(file: &Path) -> Result<Builtins, Error> {
+    let uri = Url::from_file_path(file).map_err(|_| Error::Uri { uri: file.to_string_lossy().to_string() })?;
+    let file = File::open(file)?;
+    let content = io::read_to_string(file)?;
+    let lines = content.lines();
+    Ok(HashMap::from([
+        ("Alias", Declaration {
+            location: Location { uri, range: Range {
+                start: Position { line: 36, character: 9 },
+                end:   Position { line: 36, character: 14 },
+            }},
+            documentation: MarkupContent {
+                kind: MarkupKind::Markdown,
+                value: lines.take(35)
+                    .map(|line| line.get(3..line.len()).unwrap_or("") )
+                    .fold(String::new(), |a, b| a + b + "\n")
+                ,
+            },
+        }),
+    ]))
 }
 
 fn main() -> Result<(), Error> {
@@ -36,6 +67,10 @@ fn main() -> Result<(), Error> {
         .with_target(false)
         .with_max_level(Level::TRACE)
         .init();
+
+    info!("loading builtin declarations");
+    let builtins = load_builtins(Path::new("/home/pinbraerts/src/fastbuild-lsp/builtins/alias.bff"))?;
+
     info!("starting LSP server");
 
     let (connection, io) = Connection::stdio();
@@ -48,7 +83,7 @@ fn main() -> Result<(), Error> {
     let capabilities = serde_json::to_value(&server_capabilities)?;
     let capabilities = connection.initialize(capabilities)?;
     let initialize_parameters = serde_json::from_value::<InitializeParams>(capabilities)?;
-    let server = Server { connection, server_capabilities, initialize_parameters };
+    let server = Server { connection, server_capabilities, initialize_parameters, builtins };
     server.main()?;
     io.join()?;
 
@@ -130,7 +165,7 @@ impl Server {
 
     fn goto_definition(&self, parameters: GotoDefinitionParams) -> Result<GotoDefinitionResponse, Error> {
         info!("go to definition request {:?}", parameters);
-        Ok(GotoDefinitionResponse::Array(Vec::new()))
+        Ok(GotoDefinitionResponse::Scalar(self.builtins["Alias"].location.clone()))
     }
 
 }
