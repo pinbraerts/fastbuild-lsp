4945ed9d8e7832cd7f020e759b61d921947a4f85
Dmitry Shirshov <shirshov.da@phystech.edu>
2024-03-03 10:23:46 +0300

    use tower_lsp

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use std::{fs::File, path::Path, collections::HashMap, io};
 
+use tower_lsp::lsp_types::{InitializeResult, InitializedParams, MessageType};
+use tower_lsp::{Client, LanguageServer, Server, LspService};
+use tower_lsp::lsp_types::*;
+use tower_lsp::jsonrpc::{Error, Result};
+
 use tracing::{info, debug, Level, error, trace, warn};
-use thiserror::Error;
 
-use lsp_types::{OneOf, ServerCapabilities, request::{GotoDefinition, GotoDeclarationParams, GotoDeclaration, Request, HoverRequest}, GotoDefinitionResponse, InitializeParams, GotoDefinitionParams, ClientCapabilities, DeclarationCapability, Location, Url, Range, Position, HoverParams, HoverContents, MarkupContent, MarkupKind, HoverProviderCapability, MarkedString, Hover};
-use lsp_server::{Connection, ExtractError, Message, RequestId, Response, ProtocolError, ResponseError};
-
-#[derive(Error, Debug)]
-enum Error {
+#[derive(thiserror::Error, Debug)]
+enum ParseError {
     #[error("serde_json error")]
     Json(#[from] serde_json::Error),
-    #[error("Extract error")]
-    Extract(#[from] ExtractError<lsp_server::Request>),
-    #[error("Protocol error")]
-    Protocol(#[from] ProtocolError),
     #[error("IO error")]
     IO(#[from] std::io::Error),
-    #[error("Send")]
-    Send(#[from] crossbeam_channel::SendError<Message>),
-    #[error("Unimplemented")]
-    Unimplemented { method: String },
     #[error("Uri parse")]
     Uri { uri: String },
+}
+
+impl From<ParseError> for Error {
+    fn from(val: ParseError) -> Self
+    {
+        match val {
+            ParseError::Json(error) => Error::invalid_params(error.to_string()),
+            ParseError::IO(_)       => Error::internal_error(),
+            ParseError::Uri { uri } => Error::parse_error(),
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -31,23 +35,16 @@ struct Declaration {
 }
 type Builtins = HashMap::<& 'static str, Declaration>;
 
-struct Server {
-    connection: Connection,
-    server_capabilities: ServerCapabilities,
-    initialize_parameters: InitializeParams,
-    builtins: Builtins,
-}
-
-fn load_builtins(file: &Path) -> Result<Builtins, Error> {
-    let uri = Url::from_file_path(file).map_err(|_| Error::Uri { uri: file.to_string_lossy().to_string() })?;
+fn load_builtins(file: &Path) -> std::result::Result<Builtins, ParseError> {
+    let uri = Url::from_file_path(file).map_err(|_| ParseError::Uri { uri: file.to_string_lossy().to_string() })?;
     let file = File::open(file)?;
     let content = io::read_to_string(file)?;
     let lines = content.lines();
     Ok(HashMap::from([
         ("Alias", Declaration {
             location: Location { uri, range: Range {
-                start: Position { line: 36, character: 9 },
-                end:   Position { line: 36, character: 14 },
+                start: Position { line: 42, character: 9 },
+                end:   Position { line: 42, character: 14 },
             }},
             documentation: MarkupContent {
                 kind: MarkupKind::Markdown,
@@ -60,8 +57,62 @@ fn load_builtins(file: &Path) -> Result<Builtins, Error> {
     ]))
 }
 
-fn main() -> Result<(), Error> {
-    let file = File::create("/home/pinbraerts/src/fastbuild-lsp/log.log")?;
+#[derive(Debug, Clone)]
+struct Backend {
+    client: Client,
+    builtins: Builtins,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        Ok(InitializeResult {
+            capabilities: ServerCapabilities {
+                declaration_provider: Some(DeclarationCapability::Simple(true)),
+                definition_provider: Some(OneOf::Left(true)),
+                hover_provider: Some(HoverProviderCapability::Simple(true)),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        self.client.log_message(MessageType::INFO, "server initialized!")
+        .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+
+    async fn goto_definition(&self, parameters: GotoDefinitionParams) -> Result<Option<GotoDefinitionResponse>> {
+        info!("go to definition request {:?}", parameters);
+        Ok(Some(GotoDefinitionResponse::Scalar(self.builtins["Alias"].location.clone())))
+    }
+
+    async fn goto_declaration(&self, parameters: GotoDefinitionParams) -> Result<Option<GotoDefinitionResponse>> {
+        info!("go to definition request {:?}", parameters);
+        Ok(Some(GotoDefinitionResponse::Scalar(self.builtins["Alias"].location.clone())))
+    }
+
+    async fn hover(&self, parameters: HoverParams) -> Result<Option<Hover>> {
+        info!("hover request {:?}", parameters);
+        Ok(Some(Hover {
+            contents: HoverContents::Markup(self.builtins["Alias"].documentation.clone()),
+            range: None,
+        }))
+    }
+
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let file = File::create("/home/pinbraerts/src/fastbuild-lsp/log.log").unwrap();
     tracing_subscriber::fmt()
         .with_writer(file)
         .with_target(false)
@@ -69,124 +120,13 @@ fn main() -> Result<(), Error> {
         .init();
 
     info!("loading builtin declarations");
-    let builtins = load_builtins(Path::new("/home/pinbraerts/src/fastbuild-lsp/builtins/alias.bff"))?;
+    let path = Path::new("/home/pinbraerts/src/fastbuild-lsp/builtins/alias.bff");
+    let builtins = load_builtins(path).unwrap();
 
     info!("starting LSP server");
-
-    let (connection, io) = Connection::stdio();
-
-    let server_capabilities = ServerCapabilities {
-        definition_provider: Some(OneOf::Left(true)),
-        declaration_provider: Some(DeclarationCapability::Simple(true)),
-        hover_provider: Some(HoverProviderCapability::Simple(true)),
-        ..Default::default()
-    };
-    let capabilities = serde_json::to_value(&server_capabilities)?;
-    let capabilities = connection.initialize(capabilities)?;
-    let initialize_parameters = serde_json::from_value::<InitializeParams>(capabilities)?;
-    let server = Server { connection, server_capabilities, initialize_parameters, builtins };
-    server.main()?;
-    io.join()?;
-
-    info!("shutting down LSP server");
-    Ok(())
-}
-
-fn pack<T>(id: RequestId, response: Result<T, Error>) -> Response
-where T: serde::Serialize,
-{
-    let response = match response {
-        Ok(response) => response,
-        Err(error) => {
-            return Response {
-                id, result: None, error: Some(ResponseError {
-                    code: -2,
-                    message: format!("{:?}", error),
-                    data: None,
-                })
-            };
-        }
-    };
-    match serde_json::to_value(Some(response)) {
-        Ok(result) => Response { id, result: Some(result), error: None },
-        Err(error) => Response { id, result: None, error: Some(ResponseError {
-            code: -1,
-            message: format!("serde json error: {:?}", error),
-            data: None,
-        }) },
-    }
-}
-
-impl Server {
-
-    fn process_request(&self, request: lsp_server::Request) -> Result<Response, Error> {
-        Ok(match request.method.as_str() {
-            GotoDefinition::METHOD => {
-                let (id, param) = cast::<GotoDefinition>(request)?;
-                pack(id, self.goto_definition(param))
-            },
-            GotoDeclaration::METHOD => {
-                let (id, param) = cast::<GotoDefinition>(request)?;
-                pack(id, self.goto_definition(param))
-            },
-            HoverRequest::METHOD => {
-                let (id, param) = cast::<HoverRequest>(request)?;
-                pack(id, self.hover(param))
-            },
-            _ => { return Err(Error::Unimplemented{ method: request.method }) },
-        })
-    }
-
-    fn main(self) -> Result<(), Error> {
-        info!("starting main loop");
-        for message in &self.connection.receiver {
-            info!("message received: {:?}", message);
-            match message {
-                Message::Request(request) => {
-                    if self.connection.handle_shutdown(&request)? {
-                        return Ok(());
-                    }
-                    info!("request received: {:?}", request);
-                    let responce = match self.process_request(request) {
-                        Ok(response) => response,
-                        Err(Error::Unimplemented { method }) => {
-                            warn!("Unimplemented: {}", method);
-                            continue;
-                        },
-                        Err(error) => { return Err(error); },
-                    };
-                    self.connection.sender.send(lsp_server::Message::Response(responce))?;
-                }
-                Message::Response(response) => {
-                    info!("response received: {:?}", response);
-                }
-                Message::Notification(notification) => {
-                    info!("notification received: {:?}", notification);
-                }
-            }
-        }
-        Ok(())
-    }
-
-    fn goto_definition(&self, parameters: GotoDefinitionParams) -> Result<GotoDefinitionResponse, Error> {
-        info!("go to definition request {:?}", parameters);
-        Ok(GotoDefinitionResponse::Scalar(self.builtins["Alias"].location.clone()))
-    }
-
-    fn hover(&self, parameters: HoverParams) -> Result<Hover, Error> {
-        info!("hover request {:?}", parameters);
-        Ok(Hover {
-            contents: HoverContents::Markup(self.builtins["Alias"].documentation.clone()),
-            range: None,
-        })
-    }
-
-}
-
-fn cast<R>(request: lsp_server::Request) -> Result<(RequestId, R::Params), ExtractError<lsp_server::Request>>
-where
-    R: lsp_types::request::Request,
-    R::Params: serde::de::DeserializeOwned,
-{
-    request.extract(R::METHOD)
+    let (service, socket) = LspService::new(|client| Backend {
+        client,
+        builtins,
+    });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
